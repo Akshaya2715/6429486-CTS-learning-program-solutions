de1b4a7c8e51a608524300b65224eb831f139bbe
Akshaya2715 <akshayavenkat05@gmail.com>
2025-06-22 13:40:05 +0530

    Removed unnecessary folders; kept only Week1_Handson

--- Design-patterns-principles/EX2/FactoryMethodPatternExample/WordDocument.java
+++
@@ -1,8 +0,0 @@
-package FactoryMethodPatternExample;
-
-public class WordDocument implements Document{
-    public void open(){
-        System.out.println("6429486 - Akshaya V");
-        System.out.println("Opening the Word Document");
-    }
-}
