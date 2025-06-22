8718d532d69b8c247a989014cb813012ba993494
Akshaya2715 <akshayavenkat05@gmail.com>
2025-06-22 13:36:17 +0530

    Added Week-1 documents

---
+++ Design-patterns-principles/EX2/FactoryMethodPatternExample/WordDocument.java
@@ -0,0 +1,8 @@
+package FactoryMethodPatternExample;
+
+public class WordDocument implements Document{
+    public void open(){
+        System.out.println("6429486 - Akshaya V");
+        System.out.println("Opening the Word Document");
+    }
+}
